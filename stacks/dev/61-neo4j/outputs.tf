d8bdc38a18688f3e450118e556f2b98cfb54fc9f
ytk <ytkcore@gmail.com>
2026-02-09 14:00:00 +0900

    fix: Neo4j URI에 /browser/ 경로 추가 - 303 redirect 우회
    Neo4j 루트(/)가 303 redirect를 반환하여 Teleport의
    x-teleport-auth state cookie 교환이 실패하는 문제.
    URI를 /browser/로 직접 지정하여 303을 완전 우회.

--- stacks/dev/61-neo4j/outputs.tf
+++ stacks/dev/61-neo4j/outputs.tf
@@ -26,7 +26,7 @@ output "service_endpoint" {
   description = "Service endpoint for access-gateway integration"
   value = length(aws_route53_record.neo4j) > 0 ? {
     name             = "neo4j"
-    uri              = "http://${aws_route53_record.neo4j[0].fqdn}:7474"
+    uri              = "http://${aws_route53_record.neo4j[0].fqdn}:7474/browser/"
     type             = "web"
     internal         = true
     rewrite_redirect = ["${aws_route53_record.neo4j[0].fqdn}:7474"]
